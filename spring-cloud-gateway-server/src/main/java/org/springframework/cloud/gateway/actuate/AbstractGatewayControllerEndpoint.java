9fe11bff233c50ab972b53aeffd0f0818d937c45
felipe <felipeadsc@gmail.com>
2023-05-28 21:19:10 -0300

    remove unused code

--- spring-cloud-gateway-server/src/main/java/org/springframework/cloud/gateway/actuate/AbstractGatewayControllerEndpoint.java
+++ spring-cloud-gateway-server/src/main/java/org/springframework/cloud/gateway/actuate/AbstractGatewayControllerEndpoint.java
@@ -52,7 +52,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 /**
@@ -97,7 +96,6 @@ public class AbstractGatewayControllerEndpoint implements ApplicationEventPublis
 	// TODO: Add uncommited or new but not active routes endpoint
 
 	@PostMapping("/refresh")
-	@ResponseStatus(HttpStatus.ACCEPTED)
 	public Mono<ResponseEntity<Object>> refresh(
 			@RequestParam(value = "metadata", required = false) List<String> byMetadata) {
 		publishRefreshEvent(byMetadata);
